e93c7a86c872653d10d5c74d862a869885825780
김가은 <kge02160@gmail.com>
2024-11-06 19:53:51 +0900

    chore :: UUID로 변경

--- src/main/java/com/example/west/domain/user/domain/User.java
+++ src/main/java/com/example/west/domain/user/domain/User.java
@@ -1,44 +1,50 @@
 package com.example.west.domain.user.domain;
 
-import com.example.west.domain.user.domain.type.Sex;
+import com.example.west.domain.user.domain.type.Gender;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.GenericGenerator;
 
 import javax.persistence.*;
+import java.util.UUID;
 
-@Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    @ColumnDefault("random_uuid()")
+    private UUID id;
 
-    @Column(columnDefinition = "varchar(10)", nullable = false)
+    @Column(name = "user_name", columnDefinition = "varchar(10)", nullable = false)
     private String name;
 
-    @Column(columnDefinition = "varchar(20)", nullable = false)
+    @Column(name = "user_number", columnDefinition = "varchar(20)", nullable = false)
     private String number;
-
+//    @Id
+//    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "account_id", columnDefinition = "varchar(30)", nullable = false)
     private String accountId;
 
-    @Column(columnDefinition = "varchar(100)", nullable = false)
+    @Column(name = "password", columnDefinition = "varchar(100)", nullable = false)
     private String password;
 
     @Enumerated(EnumType.STRING)
     @Column(name = "user_gender", nullable = false)
-    private Sex sex;
+    private Gender gender;
 
     @Builder
-    public User(String name, String number, String accountId, String password, Sex sex) {
+    public User(String name, String number, String accountId, String password, Gender gender) {
         this.name = name;
         this.number = number;
         this.accountId = accountId;
         this.password = password;
-        this.sex = sex;
+        this.gender = gender;
     }
 }
