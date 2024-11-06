1e2376ad54f4d0f130209d7db2a36dbb9097a4ac
김가은 <kge02160@gmail.com>
2024-11-06 19:53:51 +0900

    chore :: UUID로 변경

--- src/main/java/com/example/west/domain/user/domain/repository/UserRepository.java
+++ src/main/java/com/example/west/domain/user/domain/repository/UserRepository.java
@@ -4,8 +4,9 @@ import com.example.west.domain.user.domain.User;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
+import java.util.UUID;
 
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, UUID> {
 
     Optional<User> findByAccountId(String accountId);
 
